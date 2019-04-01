06d395f6f60ae84f4a3019c23ecf57edb67ea01c
KiNO <matrin@ttu.ee>
2019-04-01 16:38:46 +0300

    yass

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ToDoBoX</title>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link href="https://fonts.googleapis.com/css?family=Baloo|Montserrat" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col s12 m3">
+        <div class="box z-depth-1">
+          <p class="boxContent">
+            Look at this amazing note, it's so beautiful and long and I hope it doesn't take too much space.
+          </p>
+          <div class="boxBottom">
+            <p class="date">Due: 23/1/19</p>
+            <a class="btn-floating btn-small waves-effect waves-light blue check">
+              <i class="material-icons">check</i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m3">
+        <div class="box z-depth-1">
+          <p class="boxContent">
+            Look at this amazing note, it's so beautiful and long and I hope it doesn't take too much space.
+          </p>
+          <div class="boxBottom">
+            <p class="date">Due: 23/1/19</p>
+            <a class="btn-floating btn-small waves-effect waves-light blue check">
+              <i class="material-icons">check</i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m3">
+        <div class="box z-depth-1">
+          <p class="boxContent">
+            Look at this amazing note, it's so beautiful and long and I hope it doesn't take too much space.
+          </p>
+          <div class="boxBottom">
+            <p class="date">Due: 23/1/19</p>
+            <a class="btn-floating btn-small waves-effect waves-light blue check">
+              <i class="material-icons">check</i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col s12 m3">
+        <div class="box z-depth-1">
+          <p class="boxContent">
+            Look at this amazing note, it's so beautiful and long and I hope it doesn't take too much space.
+          </p>
+          <div class="boxBottom">
+            <p class="date">Due: 23/1/19</p>
+            <a class="btn-floating btn-small waves-effect waves-light blue check">
+              <i class="material-icons">check</i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col s12 m3">
+        <div class="box z-depth-1">
+          <p class="boxContent">
+            Look at this amazing note, it's so beautiful and long and I hope it doesn't take too much space.
+          </p>
+          <div class="boxBottom">
+            <p class="date">Due: 23/1/19</p>
+            <a class="btn-floating btn-small waves-effect waves-light blue check">
+              <i class="material-icons">check</i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Sidebar -->
+  <ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="https://png.pngtree.com/thumb_back/fw800/back_pic/04/56/39/595865c6a86e7a6.jpg">
+      </div>
+      <a href="#user"><img class="circle" src="img/profilepic.jpg"></a>
+      <span class="white-text name">Manueru Torinidad</span>
+    </div></li>
+    <li><a href="#!"><i class="material-icons">archive</i>Archive</a></li>
+    <li><a href="#settingsModal" class="waves-effect modal-trigger">Settings</a></li>
+    <li><a href="#!" class="waves-effect">Log Out</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">ToDoBoX</a></li>
+    <li><a class="waves-effect modal-trigger" href="#aboutUsModal">About Us</a></li>
+    <li><a class="waves-effect modal-trigger" href="#contactModal">Contact</a></li>
+  </ul>
+
+  <!-- Modal New Box -->
+  <div id="newBox" class="modal">
+    <div class="modal-content newBoxContent">
+      <h5>New Note</h5>
+      <div class="row blue-text text-darken-2">
+        <div class="input-field col s12">
+          <i class="material-icons prefix">mode_edit</i>
+          <textarea id="noteInput" class="materialize-textarea" data-length="120"></textarea>
+          <label for="noteInput">What to do?</label>
+        </div>
+        <div class="input-field col s12">
+          <i class="material-icons prefix">date_range</i>
+          <input type="date">
+          <label for="date">Due by</label>
+        </div>
+      </div>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close btn-large waves-effect waves-red btn-flat transparent">
+        <i class="material-icons">clear</i>
+      </a>
+      <a href="#!" class="modal-close btn-large waves-effect waves-green btn-flat transparent">
+        <i class="material-icons">check</i>
+      </a>
+    </div>
+  </div>
+
+    <!-- Modal Settings -->
+  <div id="settingsModal" class="modal">
+    <div class="modal-content">
+      <h5>Settings</h5>
+    </div>
+  </div>
+
+      <!-- Modal Contact -->
+  <div id="contactModal" class="modal">
+    <div class="modal-content">
+      <h5>Contact</h5>
+    </div>
+  </div>
+
+        <!-- Modal About Us -->
+  <div id="aboutUsModal" class="modal">
+    <div class="modal-content">
+      <h5>About Us</h5>
+    </div>
+  </div>
+
+
+  <a href="#" class="btn-flat btn-large waves-effect waves-light transparent logoButton sidenav-trigger" data-target="slide-out">
+  </a>
+  <a class="btn-floating btn-large waves-effect waves-light light-blue darken-4 addButton modal-trigger" href="#newBox">
+    <i class="material-icons">add</i>
+  </a>
+
+  <!--JavaScript at end of body for optimized loading-->
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
