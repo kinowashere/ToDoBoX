09e95b9b16b48dd4e9c849fede48f737cb612ed3
shyama <shyama@taltech.ee>
2019-04-02 13:52:20 +0300

    Side bar

--- index.php
+++ index.php
@@ -2,201 +2,205 @@
 <html>
 
 <head>
-  <title>ToDoBoX</title>
-  <!--Import Google Icon Font-->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <!--Import materialize.css-->
-  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
-  <!--Let browser know website is optimized for mobile-->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://fonts.googleapis.com/css?family=Baloo|Montserrat" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>ToDoBoX</title>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css?family=Baloo|Montserrat" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-  <?php
-  require 'lib/getNotes.php';
-  ?>
-  <a href="#" class="btn-flat btn-large waves-effect waves-light sidenav-trigger transparent" data-target="slide-out" id="menuButton">
-    <i class="material-icons">menu</i>
-  </a>
+    <?php
+    require 'lib/getNotes.php';
+    ?>
+    <a href="#" class="btn-flat btn-large waves-effect waves-light sidenav-trigger transparent" data-target="slide-out" id="menuButton">
+        <i class="material-icons">menu</i>
+    </a>
 
-  <div class="container">
-    <div class="row">
-      <?php
-      require_once 'boxLoadEngine.php';
-      ?>
-    </div>
-  </div>
-
-  <!-- Sidebar -->
-  <ul id="slide-out" class="sidenav">
-    <li>
-      <div class="user-view">
-        <div class="background">
-          <img src="https://png.pngtree.com/thumb_back/fw800/back_pic/04/56/39/595865c6a86e7a6.jpg">
+    <div class="container">
+        <div class="row">
+            <?php
+            require_once 'boxLoadEngine.php';
+            ?>
         </div>
-        <a href="#user"><img class="circle" src="img/profilepic.jpg"></a>
-        <span class="white-text name">Manueru Torinidad</span>
-      </div>
-    </li>
-    <li><a href="#!"><i class="material-icons">archive</i>Archive</a></li>
-    <li><a href="#settingsModal" class="waves-effect modal-trigger">Settings</a></li>
-    <li><a href="#!" class="waves-effect">Log Out</a></li>
-    <li>
-      <div class="divider"></div>
-    </li>
-    <li><a class="subheader">ToDoBoX</a></li>
-    <li><a class="waves-effect modal-trigger" href="#aboutUsModal">About Us</a></li>
-    <li><a class="waves-effect modal-trigger" href="#contactModal">Contact</a></li>
-  </ul>
-
-  <!-- Modal New Box -->
-  <form method="post" action="index.php">
-    <div id="newBox" class="modal">
-      <div class="modal-content newBoxContent">
-        <h5>New Note</h5>
-        <div class="row blue-text text-darken-2">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">mode_edit</i>
-            <textarea name="noteInput" id="noteInput" class="materialize-textarea" data-length="120"></textarea>
-            <label for="noteInput">What to do?</label>
-          </div>
-          <div class="input-field col s12">
-            <i class="material-icons prefix">date_range</i>
-            <input type="date" name="date" id="date">
-            <label for="date">Due by</label>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <a href="#!" class="modal-close btn-large waves-effect waves-red btn-flat transparent">
-          <i class="material-icons">clear</i>
-        </a>
-        <button type="submit" class="modal-close btn-large waves-effect waves-green btn-flat transparent"><i class="material-icons">check</i></button>
-      </div>
     </div>
-  </form>
-  <!-- Modal Settings -->
-  <div id="settingsModal" class="modal">
-    <div class="modal-content">
-      <h5>Settings</h5>
-      <ul class="collapsible">
+
+    <!-- Sidebar -->
+    <ul id="slide-out" class="sidenav">
         <li>
-          <div class="collapsible-header">
-            <i class="material-icons">account_box</i>
-            Change username
-          </div>
-          <div class="collapsible-body">
-            <div class="row">
-              <div class="input-field col s6">
-                <input id="new_name" type="text" class="validate" value="Manueru Torinidad">
-                <label for="new_name">Name</label>
-              </div>
+            <div class="user-view">
+                <div class="background">
+                    <img src="https://png.pngtree.com/thumb_back/fw800/back_pic/04/56/39/595865c6a86e7a6.jpg">
+                </div>
+                <a href="#user"><img class="circle" src="img/profilepic.jpg"></a>
+                <span class="white-text name">Manueru Torinidad</span>
             </div>
-            <button class="btn waves-effect waves-light" type="submit" name="action">Change
-              <i class="material-icons right">edit</i>
-            </button>
-          </div>
         </li>
+        <li><a href="#settingsModal" class="waves-effect modal-trigger"><i class="material-icons">settings</i>Settings</a></li>
+        <li><a href="#aboutUsModal" class="waves-effect modal-trigger"><i class="material-icons">supervisor_account</i>About Us</a></li>
+        <li><a href="#contactModal" class="waves-effect modal-trigger"><i class="material-icons">message</i>Contact</a></li>
+        <li><a href="#!" class="waves-effect"><i class="material-icons">power_settings_new</i>Logout</a></li>
 
         <li>
-          <div class="collapsible-header">
-            <i class="material-icons">email</i>
-            Change email address
-          </div>
-          <div class="collapsible-body">
-            <div class="row">
-              <div class="input-field col s6">
-                <input id="email" type="email" class="validate" value="sample@example.com">
-                <label for="email">Email</label>
-              </div>
-            </div>
-            <button class="btn waves-effect waves-light" type="submit" name="action">Change
-              <i class="material-icons right">edit</i>
-            </button>
-          </div>
         </li>
-        <li>
-          <div class="collapsible-header">
-            <i class="material-icons">enhanced_encryption</i>
-            Change password
-          </div>
-          <div class="collapsible-body">
-            <div class="row">
-              <div class="input-field col s6">
-                <input id="password" type="password" class="validate" required>
-                <label for="password">New Password</label>
-              </div>
-              <div class="input-field col s6">
-                <input id="password" type="password" class="validate" required>
-                <label for="password">Confirm Password</label>
-              </div>
-            </div>
-            <button class="btn waves-effect waves-light" type="submit" name="action">Change
-              <i class="material-icons right">edit</i>
-            </button>
 
-          </div>
+        <div class="divider"></div>
         </li>
-      </ul>
-    </div>
-  </div>
+        <li><a class="subheader">ToDoBoX</a></li>
+        <li><a href="#!"><i class="material-icons">check_box</i>Current</a></li>
+        <li><a href="#!"><i class="material-icons">archive</i>Archive</a></li>
+        \
+    </ul>
 
-  <!-- Modal Contact -->
-  <div id="contactModal" class="modal">
-    <div class="modal-content">
-      <h5>Contact</h5>
-      <div class="row">
-        <div class="input-field col s6">
-          <input id="new_name" type="text" class="validate" value="Manueru Torinidad" required>
-          <label for="new_name">Name</label>
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="input-field col s6">
-          <input id="email" type="email" class="validate" value="sample@example.com" required>
-          <label for="email">Contact Email</label>
-        </div>
-      </div>
-
-      <div class="row">
-        <form class="col s12">
-          <div class="row">
-            <div class="input-field col s12">
-              <textarea id="textarea" class="materialize-textarea"></textarea>
-              <label for="textarea">Message</label>
+    <!-- Modal New Box -->
+    <form method="post" action="index.php">
+        <div id="newBox" class="modal">
+            <div class="modal-content newBoxContent">
+                <h5>New Note</h5>
+                <div class="row blue-text text-darken-2">
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">mode_edit</i>
+                        <textarea name="noteInput" id="noteInput" class="materialize-textarea" data-length="120"></textarea>
+                        <label for="noteInput">What to do?</label>
+                    </div>
+                    <div class="input-field col s12">
+                        <i class="material-icons prefix">date_range</i>
+                        <input type="date" name="date" id="date">
+                        <label for="date">Due by</label>
+                    </div>
+                </div>
             </div>
-          </div>
-        </form>
-      </div>
-      <button class="btn waves-effect waves-light" type="submit" name="action">Send
-        <i class="material-icons right">send</i>
-      </button>
+            <div class="modal-footer">
+                <a href="#!" class="modal-close btn-large waves-effect waves-red btn-flat transparent">
+                    <i class="material-icons">clear</i>
+                </a>
+                <button type="submit" class="modal-close btn-large waves-effect waves-green btn-flat transparent"><i class="material-icons">check</i></button>
+            </div>
+        </div>
+    </form>
+    <!-- Modal Settings -->
+    <div id="settingsModal" class="modal">
+        <div class="modal-content">
+            <h5>Settings</h5>
+            <ul class="collapsible">
+                <li>
+                    <div class="collapsible-header">
+                        <i class="material-icons">account_box</i>
+                        Change username
+                    </div>
+                    <div class="collapsible-body">
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <input id="new_name" type="text" class="validate" value="Manueru Torinidad">
+                                <label for="new_name">Name</label>
+                            </div>
+                        </div>
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Change
+                            <i class="material-icons right">edit</i>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="collapsible-header">
+                        <i class="material-icons">email</i>
+                        Change email address
+                    </div>
+                    <div class="collapsible-body">
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <input id="email" type="email" class="validate" value="sample@example.com">
+                                <label for="email">Email</label>
+                            </div>
+                        </div>
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Change
+                            <i class="material-icons right">edit</i>
+                        </button>
+                    </div>
+                </li>
+                <li>
+                    <div class="collapsible-header">
+                        <i class="material-icons">enhanced_encryption</i>
+                        Change password
+                    </div>
+                    <div class="collapsible-body">
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <input id="password" type="password" class="validate" required>
+                                <label for="password">New Password</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <input id="password" type="password" class="validate" required>
+                                <label for="password">Confirm Password</label>
+                            </div>
+                        </div>
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Change
+                            <i class="material-icons right">edit</i>
+                        </button>
+
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 
-  <!-- Modal About Us -->
-  <div id="aboutUsModal" class="modal">
-    <div class="modal-content">
-      <h5>About Us</h5>
-      <p>We are students of TTÜ with a passion for web technologies.<br>
-        This to-do box is proudly presented by Mexi-Hon.<br>
-        Arigracias!<br>
-      </p>
-      <img class="responsive-img" src="img/mexjap.jpg">
+    <!-- Modal Contact -->
+    <div id="contactModal" class="modal">
+        <div class="modal-content">
+            <h5>Contact</h5>
+            <div class="row">
+                <div class="input-field col s6">
+                    <input id="new_name" type="text" class="validate" value="Manueru Torinidad" required>
+                    <label for="new_name">Name</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s6">
+                    <input id="email" type="email" class="validate" value="sample@example.com" required>
+                    <label for="email">Contact Email</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <form class="col s12">
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <textarea id="textarea" class="materialize-textarea"></textarea>
+                            <label for="textarea">Message</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <button class="btn waves-effect waves-light" type="submit" name="action">Send
+                <i class="material-icons right">send</i>
+            </button>
+        </div>
     </div>
-  </div>
 
+    <!-- Modal About Us -->
+    <div id="aboutUsModal" class="modal">
+        <div class="modal-content">
+            <h5>About Us</h5>
+            <p>We are students of TTÜ with a passion for web technologies.<br>
+                This to-do box is proudly presented by Mexi-Hon.<br>
+                Arigracias!<br>
+            </p>
+            <img class="responsive-img" src="img/mexjap.jpg">
+        </div>
+    </div>
 
-  <a class="btn-floating btn-large waves-effect waves-light light-blue darken-4 addButton modal-trigger" href="#newBox">
-    <i class="material-icons">add</i>
-  </a>
+    <a class="btn-floating btn-large waves-effect waves-light light-blue darken-4 addButton modal-trigger" href="#newBox">
+        <i class="material-icons">add</i>
+    </a>
 
-  <!--JavaScript at end of body for optimized loading-->
-  <script type="text/javascript" src="js/materialize.min.js"></script>
-  <script type="text/javascript" src="js/script.js"></script>
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
 </body>
 
 </html> 
