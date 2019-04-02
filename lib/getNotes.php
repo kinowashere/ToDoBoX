d1f4420c64cd06f01685ffc223503978efe04db9
KiNO <matrin@ttu.ee>
2019-04-02 12:57:37 +0300

    Corrected text echoing.

--- lib/getNotes.php
+++ lib/getNotes.php
@@ -4,14 +4,10 @@ if ($_POST) {
   $noteInput = $_POST['noteInput'];
   $date = $_POST['date'];
   $data = array("1", $date, $noteInput);
-  echo ($noteInput);
-  echo ($date);
 
   $f = fopen("./list.csv", "a");
   if ($f) {
     fputcsv($f, $data);
-  } else {
-    echo ("nofile");
   }
   fclose($f);
 }
