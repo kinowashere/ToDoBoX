a55df386d0edd706d715db7420716c07a4922ffe
KiNO <matrin@ttu.ee>
2019-04-05 13:46:23 +0300

    General update.

--- lib/loginEngine.php
+++ lib/loginEngine.php
@@ -1,7 +1,18 @@
 <?php
 
+require "lib/openConnection.php";
+
 if ($_POST) {
-  
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $sql = "select Email, Password from Users where Email='{$email}';";
+  $retval = mysqli_query($conn, $sql);
+  $userInfo = array();
+  $userInfo = mysqli_fetch_array($retval, MYSQLI_ASSOC);
+  if($userInfo["Email"] == $email and $userInfo["Password"] == $password) {
+    echo ("success!!!");
+  }
 }
 
+$conn->close();
 ?>
