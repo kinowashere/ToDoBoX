1fd0e8e8f4a1fccab04063a0c9c503cabc62feab
KiNO <matrin@ttu.ee>
2019-04-09 17:06:19 +0300

    GET WOOF

--- get.php
+++ get.php
@@ -2,7 +2,7 @@
   // GET
 
   if(isset($_GET["email_success"])) {
-    $javascript = '<script> M.toast({html: \'Email sent!\'}); </script>';
+    $javascript = '<script> M.toast({html: \'Thank you for your woof! ▼・ᴥ・▼\'}); </script>';
     echo($javascript);
   }
   ?>
