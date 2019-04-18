3d592d173f4c08835f4772395fec44de66e3d7fe
shyama <shyama@taltech.ee>
2019-04-18 15:02:00 +0300

    category

--- lib/templates/indexBoxRender.php
+++ lib/templates/indexBoxRender.php
@@ -3,16 +3,17 @@
 require_once 'vendor/autoload.php';
 $conn = open_connection();
 
-$sql = "select Valid, BoxData, BoxDate, BoxID from boxes_{$_SESSION['userID']}";
+$sql = "select Valid, BoxData, BoxDate, BoxID, BoxCategory from boxes_{$_SESSION['userID']}";
 $retval = mysqli_query($conn, $sql);
 
 $boxesArray = array();
 $counter = 0;
-while($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
+while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 	$boxesArray[$counter]["Valid"] = $row["Valid"];
 	$boxesArray[$counter]["BoxData"] = $row["BoxData"];
 	$boxesArray[$counter]["BoxDate"] = $row["BoxDate"];
 	$boxesArray[$counter]["BoxID"] = $row["BoxID"];
+	$boxesArray[$counter]["BoxCategory"] = $row["BoxCategory"];
 	$counter++;
 }
 
@@ -24,11 +25,11 @@ $today = new DateTime();
 $loader = new Twig_Loader_Filesystem('lib/templates/views');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('indexBoxViews.html',array(
-	'tasks' => $boxesArray
-)
+echo $twig->render(
+	'indexBoxViews.html',
+	array(
+		'tasks' => $boxesArray
+	)
 );
 
 close_connection($conn);
-
-?>
