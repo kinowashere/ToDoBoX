3195a7402c0cd13e1429c8f96e009f5d29b83cf4
manueltrinidad <manuel.trinidad.mo@gmail.com>
2019-04-20 12:07:08 +0300

    Updates and typos

--- lib/templates/indexBoxRender.php
+++ lib/templates/indexBoxRender.php
@@ -28,7 +28,7 @@ elseif(isset($_GET['archive'])) {
 else {
   $user_id = $_SESSION['user_id'];
   $sql = "SELECT valid, box_data, box_date, box_id, box_category 
-  FROM boxes_{$_SESSION['user_id']};";
+  FROM boxes_{$user_id};";
 }
 
 $retval = mysqli_query($conn, $sql);
@@ -45,6 +45,7 @@ while ($row = mysqli_fetch_array($retval, MYSQLI_ASSOC)) {
 }
 
 // Twig Engine
+
 $loader = new Twig_Loader_Filesystem('lib/templates/views');
 $twig = new Twig_Environment($loader);
 
